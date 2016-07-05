675b50d92c13c94362c477114b78a6fe6011b2d2
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-07-05 14:46:12 -0400

    Initial Commit. Completed normal mode.

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -1,3 +1,12 @@
-function Dog () {
-  
+function Dog (options) {
+	options = options || {};
+  this.status = (options.status !== undefined) ? options.status : "normal";
+  if(options.hungry !== undefined) {
+  	this.hungry = options.hungry;
+  } else {
+  	this.hungry = true;
+  };
+  this.color= (options.color !== undefined) ? options.color : "black";
 };
+
+export {Dog};
--- src/js/constructors/person.js
+++ src/js/constructors/person.js
@@ -1,3 +1,11 @@
-function Person () {
-  
+function Human (options) {
+	options = options || {};
+  this.pet = function(dog){
+  	dog.status = "happy";
+  };
+  this.feed = function(dog){
+  	dog.hungry = false;
+  };
+  this.cool = (options.cool !== undefined) ? options.cool : false;
 };
+export {Human};
